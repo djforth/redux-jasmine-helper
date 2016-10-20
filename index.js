1108b333b9ec01c05ed8893fcb081c09f604391e
Adrian Stainforth <adrian.stainforth@gmail.com>
2016-10-20 13:56:29 +0100

    add entry points

---
+++ index.js
@@ -0,0 +1,4 @@
+exports.actions = require('./lib/actions');
+exports.containers = require('./lib/containers')
+exports.full_reducer = require('./lib/full_reducer')
+exports.reducers = require('./lib/reducers')
